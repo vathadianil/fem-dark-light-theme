e1a640475b77f11adf8a376c36824977f8faeafc
vathadianil <anilkumarvathadi@gmail.com>
2023-08-16 19:03:46 +0530

    Header section completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <div className=" container header">
       <div className="header__title">
         <h1>Social Media Dashboard</h1>
-        Total Followers: 23,004
+        <span className="header__subtitle">Total Followers: 23,004</span>
       </div>
 
       <Toggle />
--- src/components/Header/Toggle.tsx
+++ src/components/Header/Toggle.tsx
@@ -1,22 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+const themeOptions = [
+  { id: "dark", value: "dark" },
+  { id: "system", value: "system" },
+  { id: "light", value: "light" },
+];
 function Toggle() {
+  const [theme, setTheme] = useState(themeOptions[1].value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setTheme(value);
+  };
   return (
     <fieldset
       className=" header__toggle toggle"
       aria-label="theme switcher"
       role="radiogroup"
     >
-      <label htmlFor="dark"> Dark Mode</label>
       <div className="toggle__wrapper">
-        <input type="radio" name="theme" id="dark" />
-        <input type="radio" name="theme" id="system" checked />
-        <input type="radio" name="theme" id="light" />
+        {themeOptions.map((option) => (
+          <input
+            key={option.id}
+            type="radio"
+            name="theme"
+            id={option.id}
+            value={option.value}
+            defaultChecked={theme === option.value}
+            onChange={handleChange}
+          />
+        ))}
+
         <span aria-hidden="true" className="toggle__background"></span>
         <span aria-hidden="true" className="toggle__button"></span>
       </div>
-      <label htmlFor="system"> System Pref</label>
-      <label htmlFor="light"> Liht Mode</label>
+      <label htmlFor="dark"> Dark </label>
+      <label htmlFor="system"> System</label>
+      <label htmlFor="light"> Light </label>
     </fieldset>
   );
 }
