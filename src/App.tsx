8362e248bb55f658e184afa038f023257d7dc253
vathadianil <anilkumarvathadi@gmail.com>
2023-08-16 19:16:41 +0530

    Changes done on markup and styles for header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Card from "./components/main/Card";
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
+      <header className="container header">
+        <Header />
+      </header>
+
+      <main>
+        <section className="container">
+          <Card />
+        </section>
+      </main>
     </React.Fragment>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
+import React from "react";
 import Toggle from "./Toggle";
 
 function Header() {
   return (
-    <div className=" container header">
+    <React.Fragment>
       <div className="header__title">
         <h1>Social Media Dashboard</h1>
         <span className="header__subtitle">Total Followers: 23,004</span>
       </div>
-
       <Toggle />
-    </div>
+    </React.Fragment>
   );
 }
 
